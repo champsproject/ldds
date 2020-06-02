acb778d06366094a214e122267e9b32e90d41274
Broncio <ba13026@my.bristol.ac.uk>
2020-06-02 11:41:42 +0100

    Added packaged metadata

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+PyLDs init
+"""
+
+__title__ = "pylds"
+__author__ = "Broncio Aguilar-Sanjuan, Victor-Jose Garcia-Garrido"
+__copyright__ = "Copyright 2020, PyLDs contributors"
+__license__ = "MIT"
+__version__ = "0.2.1"
+__mail__ = 'ba13026@my.bristol.ac.uk'
+__maintainer__ = __author__
+__status__ = ""
