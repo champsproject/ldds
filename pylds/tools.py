58a7d4be789efdf7f914da61df99c055a4ae7e30
vkrajnak <52881162+vkrajnak@users.noreply.github.com>
2020-07-31 15:31:09 +0200

    undo commit dcb1845

--- pylds/tools.py
+++ pylds/tools.py
@@ -7,9 +7,8 @@ Publisher, Number(Volume No), pp.142-161.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import ipywidgets as widgets
 
-def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, colormap_name='bone', colormap_mode=1, interactive=False):
+def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, norm = True, colormap_name='bone', colormap_mode=1):
     """
     Draws a Lagrangian descriptor contour plot and a contour plot showing the magnitude of its gradient field.
 
@@ -35,9 +34,6 @@ def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, color
     
     colormap_name : str, optional
         Name of matplotlib colormap for plot.
-        
-    interactive : bool
-        True allows interactively adjucting the gradient plot minimum.
     
     Returns
     -------
@@ -57,10 +53,10 @@ def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, color
     ax1_min, ax1_max, N1 = slice_parameters[0]
     ax2_min, ax2_max, N2 = slice_parameters[1]
         
-    # normalise output
-    LD = LD - np.nanmin(LD)
-    LD = LD / np.nanmax(LD)
-
+    if norm:
+        LD = LD - np.nanmin(LD)  # Scale LD output
+        LD = LD / np.nanmax(LD)  # Scale LD output
+    
     # Plot LDs
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(7.5,3), dpi=130)
     
@@ -68,10 +64,10 @@ def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, color
     points_ax2 = np.linspace(ax2_min, ax2_max, N2)
     
     if colormap_mode == 1:
-        vmin, vmax = 0, 1
+        vmin, vmax = LD.min(), LD.max()
     elif colormap_mode == 2:
-        vmin = np.nanmean(LD)-np.nanstd(LD)
-        vmax = 1
+        vmin = LD.mean()-LD.std()
+        vmax = LD.max()
     
     con0 = ax0.contourf(points_ax1, points_ax2, LD, cmap=colormap_name, vmin=vmin, vmax=vmax, levels=200)
 
@@ -96,32 +92,25 @@ def draw_lagrangian_descriptor(LD, LD_type, grid_parameters, tau, p_value, color
         string_title = ''
         print('Incorrect "LD_type". Valid options: forward, backward, total. Plot will appear without title')
     
-    fig.suptitle(string_title, fontsize=14, y=1.00)
-    plt.subplots_adjust(bottom=0.13, top=0.85)
+    fig.suptitle(string_title, fontsize=14, y=1.04)
     ax0.set_title('LD values')
     ax0.set_xlabel(slice_axes_labels[0])
     ax0.set_ylabel(slice_axes_labels[1])
     
-    ticks_LD = np.linspace(0, 1, 11)
-    fig.colorbar(con0, ax=ax0, ticks=ticks_LD, format='%.1f')
+    ticks_LD = np.linspace(np.nanmin(LD), np.nanmax(LD), 11)
+    fig.colorbar(con0, ax=ax0, ticks=ticks_LD, format='%.2f')
     
-    gradient_x, gradient_y = np.gradient(LD)
+    gradient_x, gradient_y = np.gradient( LD, 0.05, 0.05)
     gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
-    gradient_magnitude = gradient_magnitude - np.nanmin(gradient_magnitude)
-    gradient_magnitude = gradient_magnitude / np.nanmax(gradient_magnitude)
+    gradient_magnitude = gradient_magnitude/gradient_magnitude.max()
     
-    con1 = ax1.contourf(points_ax1, points_ax2, gradient_magnitude, cmap='Reds', levels=100)
-    
-    if interactive:
-        @widgets.interact(grad_minimum=(0, .99, .01))
-        def update(grad_minimum=0.0):
-            con1.set_clim(grad_minimum,1)
-            
-    ticks_gradient = np.linspace(0,1, 11)
-    fig.colorbar(con1, ax=ax1, ticks=ticks_gradient, format='%.1f')
+    con1 = ax1.contourf(points_ax1, points_ax2, gradient_magnitude, cmap='Reds', levels=200)
     ax1.set_title('LD gradient magnitude')
     ax1.set_xlabel(slice_axes_labels[0])
     ax1.label_outer()
+    
+    ticks_gradient = np.linspace(np.nanmin(gradient_magnitude), np.nanmax(gradient_magnitude), 11)
+    fig.colorbar(con1, ax=ax1, ticks=ticks_gradient, format='%.2f')
     
     plt.show()
     
