574802a53a2c66cc99db96fc0f20c890fa8b37fb
shibabrat <shiba@vt.edu>
2020-10-09 22:19:31 +0100

    Building documentation

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from distutils.core import setup
+
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+package_name = 'pyLD'
+version_num = '0.1.0'
+
+def main():
+    install_requires = ['m2r'] if on_rtd else []
+    setup(
+        name = package_name,
+
+        version = version_num,
+        
+        description = 'Python library for computing Lagrangian descriptors',
+        
+        long_description = readme,
+        
+        author = '.',
+        
+        author_email = '.',
+        
+        url = 'https://github.com/broncio123/lds',
+        
+        license = license,
+        
+        # packages=find_packages(exclude=('test','data', 'docs')),
+        packages=['pyld'],
+        package_dir={'pyld': 'pylds'},
+        # package_dir={'uposham': 'src/'},
+
+        install_requires = requirements
+    )
+
+
+if __name__ == '__main__':
+    main()
