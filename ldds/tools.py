b442301a1cf5215268ba44aab1d506c42237d43b
vkrajnak <52881162+vkrajnak@users.noreply.github.com>
2021-04-14 15:02:25 +0200

    plot max of gradients

--- ldds/tools.py
+++ ldds/tools.py
@@ -229,9 +229,11 @@ def draw_all_lds(LD_forward, LD_backward, grid_parameters, tau=np.nan, p_value=n
         if np.isnan(t_final):
             plot_title=''
         else:
-            plot_title = r'Total {}, $\tau={}$'.format(str_method,t_final)
-        LD_backward_gradient = -get_gradient_magnitude(LD_backward)
-        plot_tuple = draw_ld_pair(LD_backward+LD_forward, LD_forward_gradient+LD_backward_gradient, grid_parameters, plot_title, interactive, 'RdBu')
+            plot_title = r'Total {}, $\tau={}$'.format(str_method,t_final)mask = LD_forward_gradient>-LD_backward_gradient
+        LD_total_gradient = LD_backward_gradient
+        LD_total_gradient[mask] = LD_forward_gradient[mask]
+        LD_total_gradient[np.abs(LD_total_gradient)<0.2] = np.nan
+        plot_tuple = draw_ld_pair(LD_backward+LD_forward, LD_total_gradient, grid_parameters, plot_title, interactive, 'RdBu')
         plot_handles.append(plot_tuple)
 
     plt.show()
